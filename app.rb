f6d5178ae5545c9265d20191adb3dee0aee47b24
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 01:01:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/food' do
-    "my name is #{params[:name]}, and I love #{params[:favorite_food]}"
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
     # erb :food_form
   end
 
