db7cd101b3d1cd2cdc182a4f66e9e8f6386f777b
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 00:46:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,5 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  # Add your post route and action below
-  post '/food' do
-<<<<<<< HEAD
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-=======
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
->>>>>>> 90312bcef59ec33021cf92d28f49dd55e9e5e72b
-  end
-
+  
 end
